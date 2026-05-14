f517ece287687e079b028364bb18d267e4605fa4
Ibn3abad <abderrahim.khouk@gmail.com>
2026-05-14 10:15:52 +0200

    Version (24) 3.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ibn3abad.zakat_calculator"
         minSdk = 24
         targetSdk = 37
-        versionCode = 20
-        versionName = "3.20"
+        versionCode = 24
+        versionName = "3.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
